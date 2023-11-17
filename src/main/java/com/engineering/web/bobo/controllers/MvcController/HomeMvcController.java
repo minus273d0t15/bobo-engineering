5c5a599f047496896b4299fe9b48160993ff0ead
VasilBuhov1 <kp.buhov@gmail.com>
2023-11-17 23:46:15 +0200

    construction_quality_control.html consultancy_services.html

--- src/main/java/com/engineering/web/bobo/controllers/MvcController/HomeMvcController.java
+++ src/main/java/com/engineering/web/bobo/controllers/MvcController/HomeMvcController.java
@@ -7,23 +7,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class HomeMvcController {
-
+    //Home Page
     @GetMapping
     public String showHomePage(){
         return "index";
     }
-
+        //Кои сме ние
     @GetMapping("who_are_we")
         public String whoAreWe(){
             return "who_are_we";
         }
+        // Управление на проекти в строителството
        @GetMapping("управление-на-проекти-в-строителство")
             public String projectManagementInConstruction(){
             return "project_management_in_construction";
             }
+            // Проектиране
             @GetMapping("engineering")
         public String engineering(){
         return "engineering";
             }
+            //Консултантска-дейност
+    @GetMapping("consultancy_services")
+    public String consultancyServices(){
+        return "consultancy_services";
+    }
+    @GetMapping("/quality-control")
+    public String getQualityControl() {
+        // Implement your logic here
+        return "construction_quality_control";
+    }
     }
 
