c6d68473cf9f108f438b8ac5e1beeaf935c3355b
ARCH <minus273dot15@gmail.com>
2023-12-20 11:50:35 +0200

    Portfolio basic MVC structure

--- src/main/java/com/engineering/web/bobo/controllers/MvcController/HomeMvcController.java
+++ src/main/java/com/engineering/web/bobo/controllers/MvcController/HomeMvcController.java
@@ -1,8 +1,15 @@
 package com.engineering.web.bobo.controllers.MvcController;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.persistence.EntityNotFoundException;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
@@ -41,6 +48,32 @@ public class HomeMvcController {
     public String getQualityControl() {
         // Implement your logic here
         return "construction_quality_control";
+    }
+
+    @GetMapping("portfolio/{projectID}")
+    public String getByPhoneNumber(@PathVariable String projectID, Model model) {
+        //Temporary solution; Get path by ID to images
+        //Further migration to SQL queries expected/grep image path info by SQL, editable in admin panel/
+        if (Integer.parseInt(projectID) > 2 || Integer.parseInt(projectID) < 1) return "NotFoundView";
+        try {
+            String directoryPath = "/img/portfolio/project0"+projectID+"/";
+            File dir = new File(directoryPath);
+            File[] directoryListing = dir.listFiles();
+            List<String> imagePaths = new ArrayList<>();
+
+            if (directoryListing != null) {
+                for (File child : directoryListing) {
+                    if (child.isFile() && child.getName().toLowerCase().endsWith(".jpg")) {
+                        imagePaths.add(directoryPath + child.getName());
+                    }
+                }
+            }
+            model.addAttribute("pictures", imagePaths);
+            return "portfolio-details";
+        } catch (EntityNotFoundException e) {
+            model.addAttribute("error", e.getMessage());
+            return "NotFoundView";
+        }
     }
 }
 
